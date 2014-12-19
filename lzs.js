45e3e975d6d67c4300676849a3aad11fa839d7c7
Michael Hadley <michael.hadley@liferay.com>
2014-12-19 10:26:27 -0800

    Add basic directory structure

---
+++ lzs.js
@@ -0,0 +1,21 @@
+var lzs = new Phaser.Game(1024, 768, Phaser.AUTO, 'lzs', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+}
+
+// Game vars
+
+
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
