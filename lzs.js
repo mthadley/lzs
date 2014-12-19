3f7fada375525d550de896c2f47a2aa81d7e71a7
Michael Hadley <michael.hadley@liferay.com>
2014-12-19 12:26:46 -0800

    Re-add screen size variables

--- lzs.js
+++ lzs.js
@@ -1,4 +1,7 @@
-var lzs = new Phaser.Game(1024, 768, Phaser.AUTO, 'lzs', { preload: preload, create: create, update: update, render: render });
+var screenWidth = 1024;
+var screenHeight = 768;
+
+var lzs = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'lzs', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 	// Load sounds and sprites
