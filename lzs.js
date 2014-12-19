72f5e339eac88a82a3e0749f60d149cc1956548a
Michael Hadley <michael.hadley@liferay.com>
2014-12-19 11:19:35 -0800

    Add player sprite

--- lzs.js
+++ lzs.js
@@ -1,15 +1,24 @@
-var lzs = new Phaser.Game(1024, 768, Phaser.AUTO, 'lzs', { preload: preload, create: create, update: update, render: render });
+var screenWidth = 1024;
+var screenHeight = 768;
+
+var lzs = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'lzs', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
+	// Load sounds and sprites
+
+	lzs.load.image('ray', 'assets/sprites/player.png');
 
 }
 
 // Game vars
 
-
+var ray;
 
 function create() {
 
+	lzs.physics.startSystem(Phaser.Physics.ARCADE);
+
+	ray = lzs.add.sprite(screenWidth * 0.5, screenHeight * 0.8, 'ray');
 }
 
 function update() {
