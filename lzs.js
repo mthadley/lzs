343cc8b5cb36cb9f58b9d0d3873961abdc0f6d36
Byran Zaugg <byran.zaugg@liferay.com>
2014-12-19 14:04:06 -0800

    Beam sprite

--- lzs.js
+++ lzs.js
@@ -7,6 +7,7 @@ function preload() {
 	// Load sounds and sprites
 
 	lzs.load.image('ray', 'assets/sprites/ray-front-single.png');
+	lzs.load.image('beam', 'assets/sprites/beam-01.png');
 	lzs.load.image('background', 'assets/sprites/grass-dirt-mix-pixeled.png');
 	lzs.load.image('zombie', 'assets/sprites/placeholder_zombie.png');
 
@@ -38,7 +39,7 @@ function create() {
 	bullets = lzs.add.group();
 	bullets.enableBody = true;
 	bullets.physicsBodyType = Phaser.Physics.ARCADE;
-	bullets.createMultiple(30, 'ray');
+	bullets.createMultiple(30, 'beam');
 	bullets.setAll('anchor.x', 0.5);
 	bullets.setAll('anchor.y', 1);
 	bullets.setAll('outOfBoundsKill', true);
